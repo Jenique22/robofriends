563946f9be186936765cf6cb1af2da41cd787189
Jenique Knoesen <70643720+Jenique22@users.noreply.github.com>
2021-10-16 11:31:01 +0200

    Added react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,10 @@ import './App.css';
   function App (){
 
     const [robots, setRobots] =useState([]);
-    const[searchfield, setSearchfield] =useState('');
-  }
+    const[searchfield, setSearchfield] =useState('')
+    const [count, setCount] =useState(0)
+
+  
 
   /*ComponentDidMount() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -25,15 +27,20 @@ import './App.css';
       .then(users => {this.setState({ robots: users})});
   }*/
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users => {setRobots(users)});
+},[])
+
   const onSearchChange = (event) => {
     setSearchfield( event.target.value )
   }
-
-  {
-    
-    const filteredRobots = robots.filter(robot =>{
+  
+  const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
+    
     return !robots.length ?
       <h1>Loading</h1> :
       (
@@ -46,6 +53,7 @@ import './App.css';
         </div>
       );
   }
+  
 
 
 export default App;
